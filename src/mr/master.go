5e7708d13ba6f155974fb03d1ea3407476d3ccb2
daiguangze <daishuaishuai.z@qq.com>
2024-04-03 19:06:07 +0800

    完成mr结构体定义

--- src/mr/master.go
+++ src/mr/master.go
@@ -1,33 +1,71 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
+)
 
+type MasterTaskStatus int
+
+const (
+	Idle MasterTaskStatus = iota
+	InProcess
+	Completed
+)
+
+type State int
+
+const (
+	Map State = iota
+	Reduce
+	Exit
+	Wait
+)
 
 type Master struct {
-	// Your definitions here.
-
+	TaskQueue     chan *Task          // 等待执行的Task
+	TaskMeta      map[int]*MasterTask // 当前所有task的信息
+	MasterPhase   State
+	NReduce       int
+	InputFile     []string
+	Intermediates [][]string //Map任务产生的R个中间文件的信息
 }
+
+// master存储任务的信息, diff: 未保存wokerID.因为master不主动心跳检测
+type MasterTask struct {
+	TaskStatus    MasterTaskStatus
+	StartTime     time.Time
+	TaskReference *Task
+}
+
+type Task struct {
+	Input         string
+	TaskState     State
+	NReducer      int
+	TaskNumber    int
+	Intermediates []string
+	Output        string
+}
+
+// 大锁保平安
+var mu sync.Mutex
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (m *Master) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
-
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (m *Master) server() {
 	rpc.Register(m)
 	rpc.HandleHTTP()
@@ -41,30 +79,20 @@ func (m *Master) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrmaster.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (m *Master) Done() bool {
 	ret := false
-
 	// Your code here.
-
-
 	return ret
 }
 
-//
 // create a Master.
 // main/mrmaster.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeMaster(files []string, nReduce int) *Master {
 	m := Master{}
-
 	// Your code here.
-
-
 	m.server()
 	return &m
 }
--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,13 +6,25 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+	"time"
+)
 
 //
 // example to show how to declare the arguments
 // and reply for an RPC.
 //
+// worker  与 master 的rpc通信
+// 1. worker 在空闲时向 coordinator 发起task 请求, coordinator响应一个分配给该worker的Task
+// 2. Worker 运行完task后向coordinator 汇报
+
+const (
+	MAP    = "MAP"
+	REDUCE = "REDUCE"
+	WAIT   = "WAIT"
+)
 
 type ExampleArgs struct {
 	X int
@@ -22,8 +34,30 @@ type ExampleReply struct {
 	Y int
 }
 
-// Add your RPC definitions here.
+// 任务结构体
+// type Task struct {
+// 	Type         string // 任务类型 map reduce wait
+// 	Index        int
+// 	MapInputFile string
 
+// 	WorkerId string
+// 	Deadline time.Time
+// }
+
+// type AppluForTaskArgs struct {
+// 	WorkerId      string
+// 	LastTaskType  string
+// 	LastTaskIndex int
+// }
+
+// type ApplyForTaskReply struct {
+// 	TaskType  string
+// 	TaskIndex int
+// 	MapNum    int
+// 	ReduceNum int
+// }
+
+// Add your RPC definitions here.
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the master.
